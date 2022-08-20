1c7ad6226e33669508007165651de207eaa34054
yxhuang <yxhuang1012@gmail.com>
2022-08-20 22:50:42 +0800

    增加 intent

---
+++ app/src/androidTest/java/com/yxhuang/espresso/ui/MainActivityTest.kt
@@ -0,0 +1,68 @@
+package com.yxhuang.espresso.ui
+
+import android.app.Activity
+import android.app.Instrumentation
+import android.content.ContentResolver
+import android.content.Intent
+import android.content.res.Resources
+import android.net.Uri
+import android.provider.MediaStore
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.intent.Intents.intended
+import androidx.test.espresso.intent.Intents.intending
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasAction
+import androidx.test.espresso.intent.matcher.IntentMatchers.hasData
+import androidx.test.espresso.intent.rule.IntentsTestRule
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
+import androidx.test.platform.app.InstrumentationRegistry
+import com.yxhuang.espresso.R
+import org.hamcrest.Matcher
+import org.hamcrest.Matchers.allOf
+import org.junit.Rule
+import org.junit.Test
+import org.junit.runner.RunWith
+
+/**
+ * Created by yxhuang
+ * Date: 2022/8/20
+ * Description:
+ */
+
+@RunWith(AndroidJUnit4ClassRunner::class) class MainActivityTest {
+
+    @get:Rule
+    val intentsTestRule = IntentsTestRule(MainActivity::class.java)
+
+    @Test
+    fun test_validateIntentSentToPickPackage() {
+
+        // GIVEN
+        val expectedIntent: Matcher<Intent> = allOf(
+            hasAction(Intent.ACTION_PICK),
+            hasData(MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        )
+
+        val activityResult = createGalleryPickActivityResultStub()
+        intending(expectedIntent).respondWith(activityResult)
+
+        // Execute
+        onView(withId(R.id.button_open_gallery)).perform(click())
+        //Verify
+        intended(expectedIntent)
+    }
+
+    private fun createGalleryPickActivityResultStub(): Instrumentation.ActivityResult {
+        val resources: Resources = InstrumentationRegistry.getInstrumentation().context.resources
+        val imageUri = Uri.parse(
+            ContentResolver.SCHEME_ANDROID_RESOURCE + "://" +
+                    resources.getResourcePackageName(R.drawable.ic_launcher_background) + '/' +
+                    resources.getResourceTypeName(R.drawable.ic_launcher_background) + '/' +
+                    resources.getResourceEntryName(R.drawable.ic_launcher_background)
+        )
+        val resultIntent = Intent()
+        resultIntent.data = imageUri
+        return Instrumentation.ActivityResult(Activity.RESULT_OK, resultIntent)
+    }
+}
--- app/src/main/java/com/yxhuang/espresso/ui/MainActivity.kt
+++ app/src/main/java/com/yxhuang/espresso/ui/MainActivity.kt
@@ -1,43 +1,60 @@
 package com.yxhuang.espresso.ui
 
+import android.app.Activity
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.ImageView
+import android.widget.TextView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.RequestManager
 import com.bumptech.glide.request.RequestOptions
 import com.yxhuang.espresso.R
 import com.yxhuang.espresso.factory.MovieFragmentFactory
 
+const val GALLERY_REQUEST_CODE = 1234
+
 class MainActivity : AppCompatActivity() {
-    // dependency (typically would be injected with dagger)
-    lateinit var requestManager: RequestManager
+
+    private val TAG: String = "AppDebug"
+
+    private lateinit var image: ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        supportFragmentManager.fragmentFactory = MovieFragmentFactory()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        init()
-    }
+        image = findViewById(R.id.image)
 
-
-    private fun init(){
-        if(supportFragmentManager.fragments.size == 0){
-            val movieId = 1
-            val bundle = Bundle()
-            bundle.putInt("movie_id", movieId)
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MovieDetailFragment::class.java, bundle)
-                .commit()
+        findViewById<TextView>(R.id.button_open_gallery).setOnClickListener {
+            pickFromGallery()
         }
     }
 
-//    private fun initGlideInstance(){
-//        val requestOptions = RequestOptions
-//            .placeholderOf(R.drawable.default_image)
-//            .error(R.drawable.default_image)
-//        requestManager = Glide.with(application)
-//            .setDefaultRequestOptions(requestOptions)
-//    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if(resultCode == Activity.RESULT_OK){
+            Log.d(TAG, "RESULT_OK")
+            when(requestCode){
+
+                GALLERY_REQUEST_CODE -> {
+                    Log.d(TAG, "GALLERY_REQUEST_CODE detected.")
+                    data?.data?.let { uri ->
+                        Log.d(TAG, "URI: $uri")
+                        Glide.with(this)
+                            .load(uri)
+                            .into(image)
+                    }
+                }
+            }
+        }
+    }
+
+    private fun pickFromGallery() {
+        val intent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+        startActivityForResult(intent, GALLERY_REQUEST_CODE)
+    }
 
 }
